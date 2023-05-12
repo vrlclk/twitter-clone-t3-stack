649bdb123097e5ab7604538763f5a86de0d1f56d
vrlclk <74782116+vrlclk@users.noreply.github.com>
2023-05-12 16:28:02 +0200

    Final implementation

---
+++ src/components/ProfileImage.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { VscAccount } from "react-icons/vsc";
+
+type ProfileImageProps = {
+    src?: string | null;    // ?: optional property
+    className?: string;
+}
+
+export function ProfileImage({src, className = ""}: ProfileImageProps){
+    return(
+        <div className={`relative h-12 w-12 overflow-hidden rounded-full ${className}`}>
+            {src == null ? <VscAccount className="h-full w-full" /> : <Image src={src} alt="Profile Image" quality={100} fill />}
+        </div>
+    );
+}
